10b93d59c7e77be66ec269fb00a9cedb0702b102
Misael <qmisael1023@gmail.com>
2021-10-05 20:25:16 -0700

    EJEMPLO 1 TERMINADO

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import random
 def humanguess():
     number = random.randrange(0,10)
     print (number)
-    numero=0
+    numero=1
 
     while numero!=number:
         numero=int(input("INTRODUCE UN NUMERO "))
